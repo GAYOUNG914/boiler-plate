7631a42021edfe5b4ab887cf9049266b56ae5c72
GAYOUNG914 <rkdud91455@gmail.com>
2023-11-27 23:55:28 +0900

    bcrypt를 이용하여 패스워드 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -30,6 +32,29 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 })
+
+userSchema.pre('save',function(next){
+  let user = this; //실제 유저 데이터
+  let myPlaintextPassword = user.password; //실제 유저 데이터 내부에 있는, 유저가 던져준 패스워드
+  
+  if (user.isModified('password')){ //유저가 패스워드를 바꿨을 때만 패스워드를 해쉬화 한다
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+  
+      if(err) return next(err);
+  
+      bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+        if(err) return next(err);
+        user.password = hash;// Store hash in your password DB.
+        next();
+        });
+    });
+
+  }
+
+
+  
+
+}) //mongoose 메서드 pre()는 따옴표 내부의 메서드 이전에 어떠한 행동을 하게 하는 것임
 
 const User = mongoose.model('User', userSchema)
 
